752f50bf9b6619cbd8d4c0ca2ef1b99c153b3efa
Bindu1724 <madishettibindu1706@gmail.com>
2025-03-25 13:31:46 +0530

    Update send-email.py

--- send-email.py
+++ send-email.py
@@ -8,10 +8,10 @@ message = input("MESSAGE: ")
 
 text = f"Subject: {subject}\n\n{message}"
 
-server = smtplib.SMTP("smtp.gmail.com",587)
-server.starttls()
+server = smtplib.SMTP("smtp.gmail.com",587) #creates a connection to Gmail's SMTP server on port 587
+server.starttls() #upgrades the connection to use encryption (TLS) for secure data transmission.
 
-server.login(email, "fvlt kyzn mxjv hymq")
+server.login(email, "fvlt kyzn mxjv hymq") #logs into the sender's email account using the email address and a password or app-specific key 
 
 server.sendmail(email, reciever_email, text)
 
